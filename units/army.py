768787be5e71cefaa824bcf7b46b6c87a4873368
Sachin Rangarajan <sachin.rangarajan@gmail.com>
2023-11-09 22:10:59 -0800

    123

---
+++ units/army.py
@@ -0,0 +1,14 @@
+class Army:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+
+    def move(self, dx, dy)
+        # move the army by dx, dy steps
+        self.x += dx
+        self.y += dy
+
+    def __repr__(self):
+        return f"{self.name} ({self.x}, {self.y})"
+    
