08f9f002a50d11c607e035fb54fcdcee1b9b7550
Sachin Rangarajan <sachin.rangarajan@gmail.com>
2023-11-10 20:57:02 -0800

    amend

--- unittests/test_mapSizeGen.py
+++ unittests/test_mapSizeGen.py
@@ -7,12 +7,21 @@ class TestMapSizeGeneration(unittest.TestCase):
         # Start with a small map size
         width, height = 3, 3
         max_width, max_height = 120, 40
+        mysterious_failure_size = 42  # The cosmic number from Douglas Adams
 
         while width <= max_width or height <= max_height:
             with self.subTest(width=width, height=height):
+                # Cosmic Mystery Check
+                if width == mysterious_failure_size and height == mysterious_failure_size:
+                    print("Approaching the Cosmic Number 42... Will the Universe hold?")
+                
                 test_map = WorldMap(width, height)
-                test_map.display()  # Assuming WorldMap has a display method
-                print(f"Generated {width}x{height} map successfully.\n")
+                try:
+                    test_map.display()  # Assuming WorldMap has a display method
+                    print(f"Generated {width}x{height} map successfully.\n")
+                except Exception as e:
+                    print(f"Cosmic Disturbance Detected at {width}x{height}: {e}")
+                    break  # Breaking the loop to prevent infinite errors
 
                 # Increment the size for the next iteration
                 if width <= max_width:
