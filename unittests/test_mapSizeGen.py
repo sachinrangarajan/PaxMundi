0766fc7e7daf000d2b18bee067a0820d602cf394
Sachin Rangarajan <sachin.rangarajan@gmail.com>
2023-11-10 20:53:46 -0800

    newcode

--- unittests/test_mapSizeGen.py
+++ unittests/test_mapSizeGen.py
@@ -1,5 +1,4 @@
 import unittest
-
 from map.world_map import WorldMap
 
 class TestMapSizeGeneration(unittest.TestCase):
@@ -9,15 +8,17 @@ class TestMapSizeGeneration(unittest.TestCase):
         width, height = 3, 3
         max_width, max_height = 120, 40
 
-        while width <= max_width and height <= max_height:
+        while width <= max_width or height <= max_height:
             with self.subTest(width=width, height=height):
                 test_map = WorldMap(width, height)
                 test_map.display()  # Assuming WorldMap has a display method
                 print(f"Generated {width}x{height} map successfully.\n")
 
                 # Increment the size for the next iteration
-                width += 1
-                height = int(width * (max_height / max_width))
+                if width <= max_width:
+                    width += 1
+                if height <= max_height:
+                    height += 1
 
         # The test passes if it reaches this point without errors
         self.assertTrue(True)
