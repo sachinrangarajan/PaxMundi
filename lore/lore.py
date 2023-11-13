9df598476ab2634101a230d4c101babe3ef1a6f7
sachinrangarajan <sachin.rangarajan@gmail.com>
2023-11-12 18:31:38 -0800

    Refactoring

---
+++ lore/lore.py
@@ -0,0 +1,14 @@
+"""Journal of creation
+Sat 11/11/23 18:53: 
+
+The Guardians (Unit Tests): In our universe, these guardians are the unit tests, ensuring that only code that respects the fundamental laws (or, in our case, functional requirements) can shape the world. They are the ancient sentinels, maintaining order and preventing chaos in the cosmos of our code.
+
+The Ainur (Core Functions): These are the core functions and algorithms that 'sing' the world into existence, crafting the landscapes, populating them with entities, and setting the stage for the game's unfolding drama. Their harmonious interplay, as guided by the guardians, leads to the creation of a balanced and engaging world.
+
+The Creation Myth: In PaxMundi's lore, this process mirrors the ancient tales of creation – the guardians test and challenge the Ainur, ensuring that only a harmonious and well-structured song can bring the world into being. This myth reflects the iterative and evolutionary nature of software development, where each refinement brings us closer to our ideal vision.
+
+The Titanomachy/Deva vs. Asura (Refactoring and Evolution): The ongoing struggle, akin to the battles of ancient myths, represents the constant refinement and evolution of the code. Each refactoring is a battle, a challenge to improve and evolve, leading to a more refined and sophisticated world (or game).
+
+
+
+"""
